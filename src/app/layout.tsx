8cb79fe9812eb38f48aa5cb868ead41bf54ddafc
John Gates <sigmatoilet12skibidi@gmail.com>
2025-12-20 00:33:22 +0000

    add the mini player for this song https://open.spotify.com/track/2grjqo0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,26 @@ export default function RootLayout({
       </head>
       <body className={cn('font-body antialiased')}>
         <SoundProvider>
-          {children}
+          <div className="pb-28">
+            {children}
+          </div>
           <Toaster />
         </SoundProvider>
+        <div className="fixed bottom-0 left-0 right-0 z-50 p-2 md:p-4">
+            <div className="max-w-md mx-auto">
+                 <iframe 
+                    style={{ borderRadius: '12px' }} 
+                    src="https://open.spotify.com/embed/track/2grjqo0Frpf2okIBiifQKs?utm_source=generator&theme=0"
+                    width="100%" 
+                    height="80" 
+                    frameBorder="0" 
+                    allowFullScreen={false}
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                    loading="lazy"
+                    title="Our Song"
+                ></iframe>
+            </div>
+        </div>
       </body>
     </html>
   );
